5194513b5e6766063f423b3d1d48213373775ec3
Matt Dobson <mdobson4@gmail.com>
2014-11-23 20:57:22 -0500

    Fixing bugs.

--- cli.js
+++ cli.js
@@ -5,15 +5,21 @@ var fs = require('fs');
 var path = require('path');
 var gistify = require('./');
 var configPath = path.join(process.env.HOME, '.gistify');
+var colors = require('colors');
 var description = 'Created with Gistify';
 var pub = true;
+
+colors.setTheme({
+  error: 'red',
+  info: 'cyan'
+});
 
 if(fs.existsSync(configPath)) {
   var json = JSON.parse(fs.readFileSync(configPath));
   var token = json.token;
   gistify.authenticateToken(token, function(err) {
     if(err) {
-      console.log('Error authenticating current token ' + token + ' you should probably refresh it.');
+      console.log('Error authenticating current token ' + token + ' you should probably refresh it.'.error);
     }
   });
 }
@@ -24,7 +30,7 @@ program
   .version('0.0.1');
 
 function gistifyFile(description, pub, file) {
-  console.log('Gistifying file: ' + file);
+  console.log('[Gistify] -- Gistifying file: ' + file.info);
   if(Array.isArray(file)) {
     var obj = {};
     file.forEach(function(f) {
@@ -36,7 +42,7 @@ function gistifyFile(description, pub, file) {
   } else {
     fs.readFile(file, function(err, data) {
       if(err) {
-        console.log('Error reading file. ' + err);
+        console.log('Error reading file. ' + err.error);
       } else {
         var base = path.basename(file);
         var obj = {};
@@ -49,17 +55,18 @@ function gistifyFile(description, pub, file) {
 
 program
   .command('login <username> <password>')
+  .description('Login to github. Post gists on your behalf.')
   .action(function(username, password) {
     gistify.authenticate(username, password, {}, function(err, token) {
       if(err) {
-        console.log('Authentication Error. ' + err);
+        console.log('Authentication Error. ' + err.error);
       } else {
-        console.log('Writing access token ' + token + ' to .gistify in home directory');
+        console.log('[Gistify] -- Writing access token ' + token + ' to .gistify in home directory'.info);
         fs.writeFile(configPath, JSON.stringify({ token: token }), function(err) {
           if(err) {
-            console.log('Oops! Looks like the token wasn\'t saved. Error: ' + err);
+            console.log('Oops! Looks like the token wasn\'t saved. Error: ' + err.error);
           } else {
-            console.log('Token saved!');
+            console.log('[Gistify] -- Token saved!');
           }
         });
       }
@@ -68,11 +75,11 @@ program
 
 program
   .command('gist <file>')
+  .description('Create a new gist from a file or directory.')
   .action(function(file) {
     var currentPath = path.join(process.cwd(), file);
     fs.stat(currentPath, function(err, stat) {
-      
-      if(program.description) {
+      if(typeof program.description !== 'function') {
         description = program.description;
       }
 
@@ -81,16 +88,16 @@ program
       }
 
       if(err) {
-        console.log('Error accessing path. ' + err);
+        console.log('Error accessing path. ' + err.error);
       } else {
         if(stat.isFile()) {
-          console.log('Accessing file');
-          //gistifyFile(description, pub, currentPath);
+          console.log('[Gistify] -- Accessing file'.info);
+          gistifyFile(description, pub, currentPath);
         } else if(stat.isDirectory()) {
-          console.log('Accessing directory');
+          console.log('[Gistify] -- Accessing directory'.info);
           fs.readdir(currentPath, function(err, files) {
             if(err) {
-              console.log('Error reading files. ' + err);
+              console.log('Error reading files. ' + err.error);
             } else {
               files = files.map(function(file) {
                 return path.join(currentPath, file);
